c844c8d39949561d3bc75def8cb7f19efc68c8e2
H. op den Akker <h.opdenakker@ziggo.nl>
2025-09-20 13:49:24 +0200

    workforce reduction explained

---
+++ inhumanResources.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# NAME		inhumanResources.sh
+#
+# DESCRIPTION	https://forums.theregister.co.uk/forum/1/2017/11/27/death_taxes_dxc_job_cuts_three_of_lifes_sure_bets/
+#
+# AUTHOR	Anonymous Coward
+#
+for i = 1 to NumberOfExecutives {
+	loop while EmployeeApathy <= 0 {
+		set(ExecBonus=(guaranteed AND maximum))
+		encrypt(set(EmployeeBonus = 0))
+		call(disable.protocol(ethics))
+		for j = 1 to rnd(NumberOfEmployees( {
+			exec(dump(Employee))
+			call(html_search('CV','Resume'))
+		}
+		on error call(standard.reply('Share holder','Targets not met','Workforce pyramid'))
+	}
+}
